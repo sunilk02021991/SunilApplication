e5fc047674dc02e4a454b26cf2bbe93b1d2426b5
sunilk02021991 <sunil739738@gmail.com>
2025-02-23 06:19:43 +0530

    added to the string

--- src/demoframework/Test.java
+++ src/demoframework/Test.java
@@ -10,10 +10,15 @@ public class Test {
 		
 		char [] ch = str.toCharArray();
 		
+		
+		
 		for(int i= ch.length-1;i>=0;i--)
 		{
 			System.out.print(ch[i]);
 		}
+		
+		
+		String str2 = "rahul";
 		
 	}
 
